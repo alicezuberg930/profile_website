5e86371db2cea145d3ee63f6ee22bd7e9cec4ed0
alicezuber930 <tien23851@gmail.com>
2025-05-15 14:00:48 +0700

    r

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,16 +3,15 @@ import Link from 'next/link';
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">Future Life</h1>
       <p className="text-fd-muted-foreground">
-        Bạn có mở{' '}
+        Bạn có thể chọn vào từng Link và xem thông tin.{' '}
         <Link
           href="/docs/gia-nghia"
           className="text-fd-foreground font-semibold underline"
         >
           Gia nghĩa resort
         </Link>{' '}
-        và xem thông tin.
       </p>
     </main>
   );
