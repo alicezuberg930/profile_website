390c3f8ab33be9adf6c58790a9d0587b04c93a15
alicezuber930 <tien23851@gmail.com>
2025-05-15 14:03:56 +0700

    f

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Future Life</h1>
       <p className="text-fd-muted-foreground mb-4">
-        Bạn có thể chọn vào từng Link và xem thông tin.{' '}
+        Bạn có thể chọn vào từng Link và xem thông tin của các đối tác của Future Life.{' '}
       </p>
       <ul className='list-disc'>
         <li>
