9d6d4ea01811f9eae5004499dd568fd305750c57
alicezuber930 <tien23851@gmail.com>
2025-05-15 14:02:42 +0700

    R

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,15 +4,27 @@ export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Future Life</h1>
-      <p className="text-fd-muted-foreground">
+      <p className="text-fd-muted-foreground mb-4">
         Bạn có thể chọn vào từng Link và xem thông tin.{' '}
-        <Link
-          href="/docs/gia-nghia"
-          className="text-fd-foreground font-semibold underline"
-        >
-          Gia nghĩa resort
-        </Link>{' '}
       </p>
+      <ul className='list-disc'>
+        <li>
+          <Link
+            href="/docs/gia-nghia"
+            className="text-fd-foreground font-semibold underline"
+          >
+            Gia nghĩa resort
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/docs/gia-nghia"
+            className="text-fd-foreground font-semibold underline"
+          >
+            Kaiwan
+          </Link>
+        </li>
+      </ul>
     </main>
   );
 }
